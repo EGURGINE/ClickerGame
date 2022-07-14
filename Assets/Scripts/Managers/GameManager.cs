18e1544280dfc1e8229f2f909d0bc39ecdea4223
wtsiwon <siwon4727@gmail.com>
2022-07-14 16:06:30 +0900

    게임 마네자

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,23 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private Text effortText;
+    [SerializeField]
+    private Text perClickIncrementText;
+    [SerializeField]
+    private Text perSecondIncrementText;
+
+    private float perClickIncrement;
+    public float PerClickIncrement
+    {
+        get
+        {
+            return perClickIncrement;
+        }
+        set
+        {
+            perClickIncrement = value;
+        }
+    }
+    private float perSecondIncrement;
+    public float PerSecondIncrement
+    {
+        get
+        {
+            return perClickIncrement;
+        }
+        set
+        {
+            perClickIncrement = value;
+        }
+    }
+
+    [SerializeField]
+    private Button click;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        click.onClick.AddListener(() =>
+        {
+            Click();
+        });
+    }
+    private void Click()
+    {
+        effort += perClickIncrement;
+        //¿Ã∆Â∆Æ
+    }
+
+    #region ΩÃ±€≈Ê
     private static GameManager instance;
 
     public static GameManager Instance
     {
         get
         {
-            if(instance == null)
+            if (instance == null)
             {
                 instance = FindObjectOfType<GameManager>();
             }
             return instance;
         }
     }
-
+    #endregion
+    //≥Î∑¬(¿Œ∞‘¿” ¿Á»≠)
     private float effort;
     public float Effort
     {
@@ -28,6 +84,31 @@ public class GameManager : MonoBehaviour
         set
         {
             effort = value;
+        }
+    }
+    private float effortPerSecondProduct;
+    public float EffortPerSecondProduct
+    {
+        get
+        {
+            return effortPerSecondProduct;
+        }
+        set
+        {
+            effortPerSecondProduct = value;
+        }
+    }
+
+    private float clickPerEffort;
+    public float ClickPerEffort
+    {
+        get
+        {
+            return clickPerEffort;
+        }
+        set
+        {
+            clickPerEffort = value;
         }
     }
 }
