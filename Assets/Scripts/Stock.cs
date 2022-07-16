ee8db8b327f237b5eb7c2eca450eb80cfaac3628
Junho <sdh210202@sdh.hs.kr>
2022-07-16 20:22:37 +0900

    주식 정리

--- Assets/Scripts/Stock.cs
+++ Assets/Scripts/Stock.cs
@@ -24,18 +24,27 @@ public class Stock : MonoBehaviour
 {
     private const float CMaxRange = 200;
     private const float CMinRange = -200;
+    private LineRenderer lineRenderer => GetComponent<LineRenderer>();
 
+    [Header("타입")]
     [SerializeField] private EStockType type;
     [SerializeField] private ECycleTime typeCycle;
 
+    [Header("점")]
     [SerializeField] List<GameObject> dot = new List<GameObject>();
     [SerializeField] List<float> posY = new List<float>();
+
+    [Header("비용")]
     [SerializeField] private List<int> cost = new List<int>();
     [SerializeField] TextMeshProUGUI[] costTxt;
+
+    [Header("제목 / 버튼")]
     [SerializeField] private TextMeshProUGUI subject;
     [SerializeField] private Button buyBtn;
     [SerializeField] private Button sellBtn;
 
+    #region 소지수
+    [Header("소지수")]
     [SerializeField] TextMeshProUGUI haveTxt;
     [SerializeField] private int have;
     private int Have
@@ -47,9 +56,13 @@ public class Stock : MonoBehaviour
             haveTxt.text = have.ToString();
         }
     }
+    #endregion
+
+    #region 주기
+    [Header("주기")]
+    [SerializeField] private Slider cycleDelaySlider;
     private float delay;
 
-    [SerializeField] private Slider cycleDelaySlider;
     private float cycleDelay;
     private float CycleDelay
     {
@@ -58,25 +71,31 @@ public class Stock : MonoBehaviour
         {
             cycleDelay = value;
             cycleDelaySlider.value = cycleDelay / ((float)typeCycle);
-            if (cycleDelay >= (int)typeCycle)
-            {
-                Graph();
-            }
+
+            if (cycleDelay >= (int)typeCycle) Graph();
         }
     }
-    private LineRenderer lineRenderer => GetComponent<LineRenderer>();
+    #endregion
     private void Start()
     {
         #region 버튼
         subject.text = type.ToString();
         buyBtn.onClick.AddListener(() =>
         {
-            Buy();
+            if (GameManager.Instance.Effort > cost[cost.Count - 1])
+            {
+                Have++;
+                GameManager.Instance.Effort -= (float)cost[cost.Count - 1];
+            }
         });
 
         sellBtn.onClick.AddListener(() =>
         {
-            Sell();
+            if (have > 0)
+            {
+                Have--;
+                GameManager.Instance.Effort += (float)cost[cost.Count - 1];
+            }
         });
         #endregion
         Import();
@@ -94,18 +113,15 @@ public class Stock : MonoBehaviour
     }
     private void Import()
     {
+        Have = PlayerPrefs.GetInt(type + "Have");
+
         float nowTime = (DateTime.Now.Hour * 3600) + (DateTime.Now.Minute * 60) + DateTime.Now.Second;
         float OutTime = nowTime - PlayerPrefs.GetFloat("QuitTime");
+
         CycleDelay = PlayerPrefs.GetFloat(type + "CycleDelay");
-        Have = PlayerPrefs.GetInt(type + "Have");
-        if (cycleDelay <= OutTime)
-        {
-            delay = 0;
-        }
-        else
-        {
-            delay = ((int)typeCycle) - OutTime;
-        }
+        if (cycleDelay <= OutTime) delay = 0;
+        else delay = ((int)typeCycle) - OutTime;
+
         for (int i = 0; i < dot.Count; i++)
         {
             posY.Add(PlayerPrefs.GetFloat("DotPosY" + i));
@@ -113,62 +129,33 @@ public class Stock : MonoBehaviour
             dot[i].GetComponent<RectTransform>().localPosition += new Vector3(0, posY[i], 0);
         }
     }
-
-    private void Buy()
-    {
-        if (GameManager.Instance.Effort > cost[cost.Count - 1])
-        {
-            Have++;
-            GameManager.Instance.Effort -= (float)cost[cost.Count - 1];
-        }
-    }
-
-    private void Sell()
-    {
-        if (have > 0)
-        {
-            Have--;
-            GameManager.Instance.Effort += (float)cost[cost.Count - 1];
-        }
-    }
-
     private void Graph()
     {
         CycleDelay = 0;
         for (int i = 0; i < dot.Count; i++)
         {
-            if (i >= dot.Count - 1)
-            {
-                posY[i] = UnityEngine.Random.Range(CMinRange, CMaxRange);
-            }
-            else
-            {
-                posY[i] = posY[i + 1];
-            }
+            if (i >= dot.Count - 1) posY[i] = UnityEngine.Random.Range(CMinRange, CMaxRange);
+            else posY[i] = posY[i + 1];
+
             cost[i] = ((int)type - ((int)posY[i] * 10));
             costTxt[i].text = cost[i].ToString();
         }
-        for (int i = 0; i < dot.Count; i++)
-        {
-            dot[i].GetComponent<RectTransform>().localPosition = new Vector3(dot[i].GetComponent<RectTransform>().localPosition.x, posY[i], 0);
-        }
+        for (int i = 0; i < dot.Count; i++) dot[i].GetComponent<RectTransform>().localPosition =
+                new Vector3(dot[i].GetComponent<RectTransform>().localPosition.x, posY[i], 0);
     }
 
-    // 앱 끌때     
     private void OnApplicationQuit()
     {
-        AppQuit();
+        Save();
     }
-    private void AppQuit()
+    private void Save()
     {
-        for (int i = 0; i < dot.Count; i++)
-        {
-            PlayerPrefs.SetFloat("DotPosY" + i, posY[i]); // 나중에 게임 나가기로 이동
-        }
-            float time = (DateTime.Now.Hour * 3600) + (DateTime.Now.Minute * 60) + DateTime.Now.Second;
+        for (int i = 0; i < dot.Count; i++) PlayerPrefs.SetFloat("DotPosY" + i, posY[i]);
+
+        float time = (DateTime.Now.Hour * 3600) + (DateTime.Now.Minute * 60) + DateTime.Now.Second;
         PlayerPrefs.SetFloat("QuitTime", time);
         PlayerPrefs.SetFloat(type + "CycleDelay", cycleDelay);
-        PlayerPrefs.SetInt(type + "Have", have);
 
+        PlayerPrefs.SetInt(type + "Have", have);
     }
 }
