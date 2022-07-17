a7667f4782e824976542c6fda0432b015a96570b
wtsiwon <siwon4727@gmail.com>
2022-07-17 21:23:31 +0900

    UI만드는중

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,15 +8,16 @@ using System;
 
 public class GameManager : MonoBehaviour
 {
+    //피버 버프계수
     private const float FEVER = 1.2f;
     #region Texts
     [Header("TextMeshPro")]
     [SerializeField]
-    private TextMeshProUGUI effortText;
+    private TextMeshProUGUI effortText;//현재 실력(재화)
     [SerializeField]
-    private TextMeshProUGUI perClickIncrementText;
+    private TextMeshProUGUI perClickEffortText;//클릭당 실력증가량
     [SerializeField]
-    private TextMeshProUGUI perSecondIncrementText;
+    private TextMeshProUGUI perSecondEffortText;//초당 실력 증가량
     #endregion
     [Space(25f)]
 
@@ -29,115 +30,8 @@ public class GameManager : MonoBehaviour
     private Button clickArea;//클릭범위
     [SerializeField]
     private Slider feverSlider;//피버 게이지
-
-    public float feverTime;
-
-    private float perClickIncrement;//클릭당 증가량
-    public float PerClickIncrement
-    {
-        get
-        {
-            return perClickIncrement;
-        }
-        set
-        {
-            perClickIncrement = value;
-        }
-    }
-
-    private float perSecondIncrement;
-    public float PerSecondIncrement
-    {
-        get
-        {
-            return perSecondIncrement;
-        }
-        set
-        {
-            perSecondIncrement = value;
-        }
-    }
-
-
-
-    [SerializeField]
-    private float fever;//피버 게이지 값
-
-    [HideInInspector]
-    public bool isFeverTime;
-
-    private void Awake()
-    {
-        instance = this;
-    }
-
-    private void Start()
-    {
-        clickArea.onClick.AddListener(() =>
-        {
-            Click();
-        });
-
-        #region 방치 보상
-        float nowTime = (DateTime.Now.Hour * 3600) + (DateTime.Now.Minute * 60) + DateTime.Now.Second;
-        float OutTime = nowTime - PlayerPrefs.GetFloat("QuitTime");
-        Effort += (effortPerSecondProduct * (int)OutTime) * 0.2f;
-        #endregion
-    }
-    private void Update()
-    {
-        feverSlider.value = fever;
-        effortText.text = $"{effort}";
-        perClickIncrementText.text = $"{perClickIncrement}";
-        perSecondIncrementText.text = $"{perSecondIncrement}";
-    }
-    private void FixedUpdate()
-    {
-        FeverTime();
-    }
-    private void FeverTime()
-    {
-        while (fever > 0 && isFeverTime == true)
-        {
-            if (isFeverTime == false && fever <= 0)
-            {
-                break;
-            }
-            else
-            {
-                fever -= Time.deltaTime;
-            }
-        }
-    }
-    private void Click()
-    {
-        Debug.Log(isFeverTime);
-        float perClickdeEffortfault = clickPerEffort;
-        if (isFeverTime == false)
-        {
-            effort += perClickdeEffortfault;
-            fever += 1;
-        }
-        else if (isFeverTime == true)
-        {
-            effort += (perClickIncrement * FEVER);
-        }
-    }
-    #region 싱글톤
-    private static GameManager instance;
-
-    public static GameManager Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = FindObjectOfType<GameManager>();
-            }
-            return instance;
-        }
-    }
-    #endregion
+    [Space(10f)]
+    
 
     #region 노력s
     //노력(인게임 재화)
@@ -152,6 +46,7 @@ public class GameManager : MonoBehaviour
         set
         {
             effort = value;
+            
         }
     }
 
@@ -169,17 +64,112 @@ public class GameManager : MonoBehaviour
         }
     }
     [SerializeField]
-    private float clickPerEffort;//클릭당 실력(재화)
-    public float ClickPerEffort
+    private float clickPerEffortProduct;//클릭당 실력(재화)
+    public float ClickPerEffortProduct
     {
         get
         {
-            return clickPerEffort;
+            return clickPerEffortProduct;
         }
         set
         {
-            clickPerEffort = value;
+            clickPerEffortProduct = value;
         }
     }
     #endregion
+    [SerializeField]
+    private float fever;//피버 게이지 값
+
+    public float Fever
+    {
+        get
+        {
+            return fever;
+        }
+        set
+        {
+            fever = value;
+            if(fever >= 99)
+            {
+                isFeverTime = true;
+            }
+            feverSlider.value = fever;
+        }
+    }
+
+    [HideInInspector]
+    public bool isFeverTime;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        clickArea.onClick.AddListener(() =>
+        {
+            Click();
+        });
+        
+        StartCoroutine(CPerSecondEffortProduct());
+        #region 방치 보상
+        float nowTime = (DateTime.Now.Hour * 3600) + (DateTime.Now.Minute * 60) + DateTime.Now.Second;
+        float OutTime = nowTime - PlayerPrefs.GetFloat("QuitTime");
+        Effort += (effortPerSecondProduct * (int)OutTime) * 0.2f;
+        #endregion
+    }
+    private IEnumerator CPerSecondEffortProduct()
+    {
+        yield return new WaitForSeconds(1f);
+        Effort += effortPerSecondProduct;
+        StartCoroutine(CPerSecondEffortProduct());
+    }
+    private void Update()
+    {
+        effortText.text = $"{effort}";
+        perClickEffortText.text = $"{clickPerEffortProduct}";
+        perSecondEffortText.text = $"{effortPerSecondProduct}";
+    }
+    private void FixedUpdate()
+    {
+        FeverTime();
+    }
+    private void FeverTime()
+    {
+        if(isFeverTime == true)
+        {
+            Fever -= Time.deltaTime;
+        }
+    }
+    private void Click()
+    {
+        if (isFeverTime == false)
+        {
+            effort += clickPerEffortProduct;
+            Fever += 1;
+        }
+        else if (isFeverTime == true)
+        {
+            effort += (clickPerEffortProduct * FEVER);
+        }
+        //effortAnim.Play();
+
+    }
+    #region 싱글톤
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+            return instance;
+        }
+    }
+    #endregion
+
 }
